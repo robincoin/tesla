9b10777a9d3f2b178b3333eea87d8ee68c919862
shiming.liu <shiming.liu@bkjk.com>
2019-05-29 14:06:10 +0800

    add circuibreaker

--- tesla-filter/src/main/java/io/github/tesla/filter/endpoint/plugin/request/CircuitBreakerRequestPlugin.java
+++ tesla-filter/src/main/java/io/github/tesla/filter/endpoint/plugin/request/CircuitBreakerRequestPlugin.java
@@ -1,5 +1,7 @@
 package io.github.tesla.filter.endpoint.plugin.request;
 
+import java.util.concurrent.TimeUnit;
+
 import com.hazelcast.core.IMap;
 
 import io.github.tesla.filter.AbstractRequestPlugin;
@@ -41,9 +43,10 @@ public class CircuitBreakerRequestPlugin extends AbstractRequestPlugin {
             circuitBreaker = CIRCUITBREAKER_CACHE.get(uri);
             circuitBreaker.reset(failRateForClose, idleTimeForOpen, passRateForHalfOpen, failNumForHalfOpen);
         } else {
+            // 熔断只一天生效？ 如果永久不失效的话，hazelcast的内存怎么清理？有一些问题
             circuitBreaker =
                 new HazelCastCircuitBreaker(failRateForClose, idleTimeForOpen, passRateForHalfOpen, failNumForHalfOpen);
-            CIRCUITBREAKER_CACHE.put(uri, circuitBreaker);
+            CIRCUITBREAKER_CACHE.put(uri, circuitBreaker, 1, TimeUnit.DAYS);
         }
         servletRequest.setAttribute("_CircuitBreaker", circuitBreaker);
         if (circuitBreaker.canPassCheck()) {
