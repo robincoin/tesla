89372b22e5201648b6c58fcffd162af511caaf80
shiming <ming@binance.com>
2019-06-18 16:54:47 +0800

    finish circuitbreaker

--- tesla-filter/src/main/java/io/github/tesla/filter/endpoint/definition/CircuitBreakerDefinition.java
+++ tesla-filter/src/main/java/io/github/tesla/filter/endpoint/definition/CircuitBreakerDefinition.java
@@ -1,74 +1,62 @@
 package io.github.tesla.filter.endpoint.definition;
 
-import org.apache.commons.lang3.StringUtils;
-
-import com.google.common.base.Preconditions;
-
 import io.github.tesla.common.dto.ServiceDTO;
 import io.github.tesla.filter.service.definition.PluginDefinition;
-import io.github.tesla.filter.utils.JsonUtils;
 
 public class CircuitBreakerDefinition extends PluginDefinition {
 
-    private String failRateForClose;
+	private Integer failureRateThreshold;
 
-    private Integer idleTimeForOpen;
+	private Integer waitDurationInOpenState;
 
-    private String passRateForHalfOpen;
+	private Integer ringBufferSizeInHalfOpenState;
 
-    private Integer failNumForHalfOpen;
+	private Integer ringBufferSizeInClosedState;
 
-    private String fallback;
+	private String fallback;
 
-    public String getFailRateForClose() {
-        return failRateForClose;
-    }
+	public Integer getFailureRateThreshold() {
+		return failureRateThreshold;
+	}
 
-    public void setFailRateForClose(String failRateForClose) {
-        this.failRateForClose = failRateForClose;
-    }
+	public void setFailureRateThreshold(Integer failureRateThreshold) {
+		this.failureRateThreshold = failureRateThreshold;
+	}
 
-    public Integer getIdleTimeForOpen() {
-        return idleTimeForOpen;
-    }
+	public Integer getWaitDurationInOpenState() {
+		return waitDurationInOpenState;
+	}
 
-    public void setIdleTimeForOpen(Integer idleTimeForOpen) {
-        this.idleTimeForOpen = idleTimeForOpen;
-    }
+	public void setWaitDurationInOpenState(Integer waitDurationInOpenState) {
+		this.waitDurationInOpenState = waitDurationInOpenState;
+	}
 
-    public String getPassRateForHalfOpen() {
-        return passRateForHalfOpen;
-    }
+	public Integer getRingBufferSizeInHalfOpenState() {
+		return ringBufferSizeInHalfOpenState;
+	}
 
-    public void setPassRateForHalfOpen(String passRateForHalfOpen) {
-        this.passRateForHalfOpen = passRateForHalfOpen;
-    }
+	public void setRingBufferSizeInHalfOpenState(Integer ringBufferSizeInHalfOpenState) {
+		this.ringBufferSizeInHalfOpenState = ringBufferSizeInHalfOpenState;
+	}
 
-    public Integer getFailNumForHalfOpen() {
-        return failNumForHalfOpen;
-    }
+	public Integer getRingBufferSizeInClosedState() {
+		return ringBufferSizeInClosedState;
+	}
 
-    public void setFailNumForHalfOpen(Integer failNumForHalfOpen) {
-        this.failNumForHalfOpen = failNumForHalfOpen;
-    }
+	public void setRingBufferSizeInClosedState(Integer ringBufferSizeInClosedState) {
+		this.ringBufferSizeInClosedState = ringBufferSizeInClosedState;
+	}
 
-    public String getFallback() {
-        return fallback;
-    }
+	public String getFallback() {
+		return fallback;
+	}
 
-    public void setFallback(String fallback) {
-        this.fallback = fallback;
-    }
+	public void setFallback(String fallback) {
+		this.fallback = fallback;
+	}
 
-    @Override
-    public String validate(String paramJson, ServiceDTO serviceDTO) {
-        CircuitBreakerDefinition definition = JsonUtils.fromJson(paramJson, CircuitBreakerDefinition.class);
-        Preconditions.checkArgument(StringUtils.isNotBlank(definition.getFailRateForClose()), "默认多少分钟内，失败多少次进入打开状态");
-        Preconditions.checkArgument(StringUtils.isNotBlank(definition.getPassRateForHalfOpen()), "默认多少分钟内，放多少次请求去试探");
-        Preconditions.checkArgument(definition.getIdleTimeForOpen() != null, "在熔断器打开的情况下，熔断多少秒进入半开状态");
-        Preconditions.checkArgument(definition.getFailNumForHalfOpen() != null,
-            "在熔断器半开的情况下, 试探期间，如果有超过多少次失败的，重新进入熔断打开状态，否者进入熔断关闭状态");
-        Preconditions.checkArgument(StringUtils.isNotBlank(definition.getFallback()), "熔断情况下返回的Mock数据");
-        return paramJson;
-    }
+	@Override
+	public String validate(String paramJson, ServiceDTO serviceDTO) {
+		return paramJson;
+	}
 }
--- tesla-filter/src/main/java/io/github/tesla/filter/endpoint/plugin/request/CircuitBreakerRequestPlugin.java
+++ tesla-filter/src/main/java/io/github/tesla/filter/endpoint/plugin/request/CircuitBreakerRequestPlugin.java
@@ -1,12 +1,16 @@
 package io.github.tesla.filter.endpoint.plugin.request;
 
-import com.hazelcast.core.IMap;
+import java.time.Duration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import io.github.tesla.filter.AbstractRequestPlugin;
 import io.github.tesla.filter.endpoint.definition.CircuitBreakerDefinition;
 import io.github.tesla.filter.support.annnotation.EndpointRequestPlugin;
-import io.github.tesla.filter.support.circuitbreaker.CircuitBreaker;
-import io.github.tesla.filter.support.circuitbreaker.HazelCastCircuitBreaker;
 import io.github.tesla.filter.support.servlet.NettyHttpServletRequest;
 import io.github.tesla.filter.utils.JsonUtils;
 import io.github.tesla.filter.utils.PluginUtil;
@@ -15,47 +19,46 @@ import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpUtil;
 
-@EndpointRequestPlugin(filterType = "CircuitBreakerRequestPlugin", definitionClazz = CircuitBreakerDefinition.class,
-    filterOrder = 13, filterName = "熔断插件")
+@EndpointRequestPlugin(filterType = "CircuitBreakerRequestPlugin", definitionClazz = CircuitBreakerDefinition.class, filterOrder = 13, filterName = "熔断插件")
 public class CircuitBreakerRequestPlugin extends AbstractRequestPlugin {
 
-    private static IMap<String, CircuitBreaker> CIRCUITBREAKER_CACHE;
+	private static final Logger logger = LoggerFactory.getLogger(CircuitBreakerRequestPlugin.class);
 
-    @Override
-    public HttpResponse doFilter(NettyHttpServletRequest servletRequest, HttpObject realHttpObject,
-        Object filterParam) {
-        CircuitBreakerDefinition definition = JsonUtils.json2Definition(filterParam, CircuitBreakerDefinition.class);
-        if (definition == null) {
-            return null;
-        }
-        if (CIRCUITBREAKER_CACHE == null) {
-            CIRCUITBREAKER_CACHE = getHazelcastInstance().getMap("circuitBreakerMap");
-        }
-        String uri = servletRequest.getRequestURI();
-        CircuitBreaker circuitBreaker = null;
-        final String failRateForClose = definition.getFailRateForClose();
-        final int idleTimeForOpen = definition.getIdleTimeForOpen();
-        final String passRateForHalfOpen = definition.getPassRateForHalfOpen();
-        final int failNumForHalfOpen = definition.getFailNumForHalfOpen();
-        if (CIRCUITBREAKER_CACHE.containsKey(uri)) {
-            circuitBreaker = CIRCUITBREAKER_CACHE.get(uri);
-            circuitBreaker.reset(failRateForClose, idleTimeForOpen, passRateForHalfOpen, failNumForHalfOpen);
-        } else {
-            // 不能大规模熔断，否则hazelcast吃不消
-            circuitBreaker =
-                new HazelCastCircuitBreaker(failRateForClose, idleTimeForOpen, passRateForHalfOpen, failNumForHalfOpen);
-            CIRCUITBREAKER_CACHE.put(uri, circuitBreaker);
-        }
-        servletRequest.setAttribute("_CircuitBreaker", circuitBreaker);
-        if (circuitBreaker.canPassCheck()) {
-            return null;
-        } else {
-            // 被熔断了，返回定义Mock数据出去
-            HttpResponse response = PluginUtil.createResponse(HttpResponseStatus.OK, servletRequest.getNettyRequest(),
-                definition.getFallback());
-            HttpUtil.setKeepAlive(response, false);
-            return response;
-        }
-    }
+	@Override
+	public HttpResponse doFilter(NettyHttpServletRequest servletRequest, HttpObject realHttpObject,
+			Object filterParam) {
+		CircuitBreakerDefinition definition = JsonUtils.json2Definition(filterParam, CircuitBreakerDefinition.class);
+		if (definition == null) {
+			return null;
+		}
+		String uri = servletRequest.getRequestURI();
+		Integer failureRateThreshold = definition.getFailureRateThreshold();
+		Integer waitDurationInOpenState = definition.getWaitDurationInOpenState();
+		Integer ringBufferSizeInHalfOpenState = definition.getRingBufferSizeInHalfOpenState();
+		Integer ringBufferSizeInClosedState = definition.getRingBufferSizeInClosedState();
+		CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()//
+				.failureRateThreshold(failureRateThreshold)// 失败率阈值
+				.waitDurationInOpenState(Duration.ofMillis(waitDurationInOpenState))// 用来指定断路器从OPEN到HALF_OPEN状态等待的时长
+				.ringBufferSizeInHalfOpenState(ringBufferSizeInHalfOpenState)// 设置当断路器处于HALF_OPEN状态下的ring
+																				// buffer的大小，它存储了最近一段时间请求的成功失败状态
+				.ringBufferSizeInClosedState(ringBufferSizeInClosedState)// 设置当断路器处于CLOSED状态下的ring
+																			// buffer的大小，它存储了最近一段时间请求的成功失败状态
+				.enableAutomaticTransitionFromOpenToHalfOpen()// 当waitDurationInOpenState时间一过，是否自动从OPEN切换到HALF_OPEN
+				.build();
+		CircuitBreaker circuitBreaker = CircuitBreakerRegistry.ofDefaults().circuitBreaker(uri, circuitBreakerConfig);
+		boolean acquirePermission = circuitBreaker.tryAcquirePermission();
+		long start = System.nanoTime();
+		servletRequest.setAttribute("_CircuitBreakerStart", start);
+		if (acquirePermission) {
+			return null;
+		} else {
+			logger.warn("circuitBreaker is triggered, will return fallback data,the data is:{}",
+					definition.getFallback());
+			HttpResponse response = PluginUtil.createResponse(HttpResponseStatus.OK, servletRequest.getNettyRequest(),
+					definition.getFallback());
+			HttpUtil.setKeepAlive(response, false);
+			return response;
+		}
+	}
 
 }
