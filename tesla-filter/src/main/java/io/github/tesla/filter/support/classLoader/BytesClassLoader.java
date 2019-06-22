e2789e780a27685272f01230c91f04c5e0078576
shiming <ming@binance.com>
2019-06-23 03:50:27 +0800

    优化classloader

--- tesla-filter/src/main/java/io/github/tesla/filter/support/classLoader/BytesClassLoader.java
+++ tesla-filter/src/main/java/io/github/tesla/filter/support/classLoader/BytesClassLoader.java
@@ -19,7 +19,7 @@ public class BytesClassLoader extends ClassLoader {
     }
 
     public BytesClassLoader(byte[] jarBytes) {
-        this(jarBytes, BytesClassLoader.class.getClassLoader());
+        this(jarBytes, org.springframework.util.ClassUtils.getDefaultClassLoader());
     }
 
     public BytesClassLoader(byte[] jarBytes, ClassLoader parent) {
