24e14fdfab5244297eaeab4f235d5c2967a8e7b3
shiming <ming@binance.com>
2019-06-22 15:21:19 +0800

    fix bug for server.port

--- tesla-ops/bin/start.sh
+++ tesla-ops/bin/start.sh
@@ -11,5 +11,4 @@ JAVA_OPTS="-XX:+ParallelRefProcEnabled $JAVA_OPTS "
 JAVA_OPTS="-XX:+CMSScavengeBeforeRemark $JAVA_OPTS"
 JAVA_OPTS="${JAVA_OPTS} -Dsun.net.client.defaultConnectTimeout=10000"
 JAVA_OPTS="${JAVA_OPTS} -Dsun.net.client.defaultReadTimeout=30000"
-JAVA_OPTS="${JAVA_OPTS} -Dserver.port=9000"
 java $JAVA_OPTS -jar ./app.jar
