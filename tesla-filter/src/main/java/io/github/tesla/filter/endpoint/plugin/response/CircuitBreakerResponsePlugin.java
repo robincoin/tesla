97f2cd0a84bd822956f5397d542d53b5f21be236
shiming <ming@binance.com>
2019-06-24 11:16:14 +0800

    fix bug

--- tesla-filter/src/main/java/io/github/tesla/filter/endpoint/plugin/response/CircuitBreakerResponsePlugin.java
+++ tesla-filter/src/main/java/io/github/tesla/filter/endpoint/plugin/response/CircuitBreakerResponsePlugin.java
@@ -9,11 +9,11 @@ import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import io.github.tesla.filter.AbstractResponsePlugin;
 import io.github.tesla.filter.endpoint.definition.CircuitBreakerDefinition;
-import io.github.tesla.filter.support.annnotation.EndpointRequestPlugin;
+import io.github.tesla.filter.support.annnotation.EndpointResponsePlugin;
 import io.github.tesla.filter.support.servlet.NettyHttpServletRequest;
 import io.netty.handler.codec.http.HttpResponse;
 
-@EndpointRequestPlugin(filterType = "CircuitBreakerRequestPlugin", definitionClazz = CircuitBreakerDefinition.class,
+@EndpointResponsePlugin(filterType = "CircuitBreakerRequestPlugin", definitionClazz = CircuitBreakerDefinition.class,
     filterOrder = 13, filterName = "熔断插件")
 public class CircuitBreakerResponsePlugin extends AbstractResponsePlugin {
 
