52ff033dba23f21c52722c012992fde8055bcdd5
shiming <ming@binance.com>
2019-06-24 23:48:24 +0800

    update

--- tesla-ops/bin/start.sh
+++ tesla-ops/bin/start.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
-JAVA_OPTS="-Xmx2688M -Xms2688M -Xmn960M $JAVA_OPTS "
-JAVA_OPTS="-XX:MaxMetaspaceSize=512M $JAVA_OPTS "
-JAVA_OPTS="-XX:MetaspaceSize=512M $JAVA_OPTS "
-JAVA_OPTS="-XX:+UseConcMarkSweepGC $JAVA_OPTS "
-JAVA_OPTS="-XX:+UseCMSInitiatingOccupancyOnly $JAVA_OPTS "
-JAVA_OPTS="-XX:CMSInitiatingOccupancyFraction=70 $JAVA_OPTS "
-JAVA_OPTS="-XX:+ExplicitGCInvokesConcurrentAndUnloadsClasses $JAVA_OPTS "
-JAVA_OPTS="-XX:+CMSClassUnloadingEnabled $JAVA_OPTS "
-JAVA_OPTS="-XX:+ParallelRefProcEnabled $JAVA_OPTS "
-JAVA_OPTS="-XX:+CMSScavengeBeforeRemark $JAVA_OPTS"
+JAVA_OPTS="-Xmx640M -Xms640M -Xmn192M $JAVA_OPTS "
+JAVA_OPTS="-XX:MaxMetaspaceSize=128M $JAVA_OPTS "
+JAVA_OPTS="-XX:MetaspaceSize=128M $JAVA_OPTS "
+JAVA_OPTS="-XX:+UseSerialGC $JAVA_OPTS "
 JAVA_OPTS="${JAVA_OPTS} -Dsun.net.client.defaultConnectTimeout=10000"
 JAVA_OPTS="${JAVA_OPTS} -Dsun.net.client.defaultReadTimeout=30000"
 java $JAVA_OPTS -jar ./app.jar
