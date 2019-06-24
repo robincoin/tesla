e964cd91dba7686bb7fc0e3c90788fc80df7f206
shiming <ming@binance.com>
2019-06-24 10:12:25 +0800

    fix

--- tesla-filter/src/main/java/io/github/tesla/filter/support/plugins/ResponsePluginMetadata.java
+++ tesla-filter/src/main/java/io/github/tesla/filter/support/plugins/ResponsePluginMetadata.java
@@ -4,7 +4,6 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.WeakHashMap;
 
-import io.github.tesla.filter.AbstractRequestPlugin;
 import io.github.tesla.filter.AbstractResponsePlugin;
 
 @SuppressWarnings({"unchecked"})
